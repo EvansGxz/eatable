74181982ded3946f738f5f384b34c87a65b973dd
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-04-06 17:49:46 -0500

    Add typography

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,69 +1,64 @@
 export const fonts = {
-  primary: `"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-  Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
-  secondary: `"Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
-  Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
+  primary: `'Source Sans Pro', sans-serif`,
+  secondary: `"Lato", , sans-serif`,
 };
 
 export const typography = {
-  text: {
+  regular: {
     xs: `
-    font-size: 0.75rem;
-    line-height: 1rem;
-    `,
+      font-size: 0.875rem;
+      line-height: 1.05rem;
+      `,
     sm: `
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    `,
+      font-size: 1rem;
+      line-height: 1.257rem;
+      `,
     md: `
-    font-size: 1rem;
-    line-height: 1.5rem;
-    `,
+      font-size: 1.125rem;
+      line-height: 1.414rem;
+      `,
     lg: `
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-    `,
+      font-size: 1.375rem;
+      line-height: 1.728rem;
+      `,
     xl: `
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    `,
+      font-size: 1.75rem;
+      line-height: 2.2rem;
+      `,
   },
-  head: {
-    xs: `
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    `,
-    sm: `
-    font-size: 1.5rem;
-    line-height: 2rem;
-    `,
-    md: `
-    font-size: 1.875rem;
-    line-height: 2.25rem;
-    `,
-    lg: `
-    font-size: 2.25rem;
-    line-height: 2.5rem;
-    `,
-    xl: `
-    font-size: 3rem;
-    line-height: 3rem;
-    `,
-    xxl: `
-    font-size: 3.75rem;
-    line-height: 3.75rem;
-    `,
+  semibold: {
+      xs: `
+        font-size: 0.875rem;
+        line-height: 1.05rem;
+        `,
+      sm: `
+        font-size: 1rem;
+        line-height: 1.257rem;
+        `,
+      md: `
+        font-size: 1.125rem;
+        line-height: 1.414rem;
+        `,
+      lg: `
+        font-size: 1.375rem;
+        line-height: 1.728rem;
+        `,
+      xl: `
+        font-size: 1.75rem;
+        line-height: 2.2rem;
+        `,
   },
 };
 
-for (const size in typography.text) {
-  typography.text[size] += `
+for (const size in typography.regular) {
+  typography.regular[size] += `
   font-family: ${fonts.primary};
+  font-weight: 400;
   `;
 }
 
-for (const size in typography.head) {
-  typography.head[size] += `
+for (const size in typography.semibold) {
+  typography.semibold[size] += `
   font-family: ${fonts.secondary};
   font-weight: 600;
   `;
