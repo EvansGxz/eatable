ea4d638c40e3965c009b8fe4e25bbda9f81856bb
rubencito <ruben.santillan0902@gmail.com>
2022-04-06 18:32:31 -0500

    add food card

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,5 +1,18 @@
+import FoodCard from "../components/cards/food-card";
+
 function HomePage() {
-  return <h1>HOME PAGE</h1>;
+  return (
+    <div>
+      <h1> HOME PAGE</h1>
+      <FoodCard
+        src={
+          "https://img.freepik.com/free-photo/top-view-green-cream-soups_23-2148519096.jpg"
+        }
+        name={"Pollo con papas"}
+        price={54.45}
+      />
+    </div>
+  );
 }
 
 export default HomePage;
